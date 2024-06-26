f1d1692f9661d7351ddbe9c762fa885a8e9c0b50
= <mclaughlinambrose@gmail.com>
2024-06-26 19:54:31 -0400

    Added Basic yt-dlp script updated permissions

--- install_ytdlp.sh
+++ install_ytdlp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Download yt-dlp binary
-wget -O yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+curl -L -o yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
 chmod +x yt-dlp
