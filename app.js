4f07003e7039bae68c3b5e7f9415def3f6d48a2a
Ambrose Mclaughlin <mclaughlinambrose@gmail.com>
2024-08-08 21:59:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ var {photoAddGradientAndText,photoToVideoPostToS3,videoToVideoPost,downloadFile}
 //videoToVideoPost('https://img-9gag-fun.9cache.com/photo/anzL5dE_460sv.mp4',"xxxxxxxxxxxxxxxxorschel shines in rain to lead Open after brutal third round",'99','TEXT','Crazy')
 //Testing This in large quantities
 photoToVideoPostToS3('http://www.yardbarker.com/media/e/1/e157a33aef78f1d2f9d8108d13d5d24d59cac348/thumb_16x9/USATSI_23326796_168404824_lowres-1024x683.jpg',"Horschel shines in rain to lead Open after brutal third round",String(i),'text','Breaking',true)
-try{
+//try{
 // for(var i =10; i <25; i++){
 //    videoToVideoPost('https://img-9gag-fun.9cache.com/photo/anzL5dE_460sv.mp4',"xxxxxxxxxxxxxxxxorschel shines in rain to lead Open after brutal third round",String(i),'TEXT','Crazy')}
 // photoToVideoPostToS3('http://www.yardbarker.com/media/e/1/e157a33aef78f1d2f9d8108d13d5d24d59cac348/thumb_16x9/USATSI_23326796_168404824_lowres-1024x683.jpg',"Horschel shines in rain to lead Open after brutal third round",String(i),'text','Breaking',true)
